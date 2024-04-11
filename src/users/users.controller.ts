9e1a4420e18ca04b1069607c5e9a5d2b58d2a145
Odradex <sivakov.daniil@gmail.com>
2024-04-11 19:00:26 +0300

    Setup user signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
 import { Controller } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+
+@Controller('auth')
+export class UsersController {
+  constructor(private usersService: UsersService) {}
+
+  @Post('/signup')
+  async createUser(@Body() user: CreateUserDto) {
+    console.log(user)
+    //return this.usersService.createUser(user);
+  }
+}
